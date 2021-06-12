1d96125f4b08e998da108e800f29fa7e322d4914
Ezra-H <huangrh9@gmail.com>
2021-06-12 18:17:16 +0800

    update

--- Megatron-LM-v1.1.5-3D_parallelism/megatron/configs/dict2cfg.py
+++ Megatron-LM-v1.1.5-3D_parallelism/megatron/configs/dict2cfg.py
@@ -9,3 +9,14 @@ def dictToObj(dictObj):
     for k, v in dictObj.items():
         d[k] = dictToObj(v)
     return d
+
+
+if __name__ == "__main__":
+    test = {"arg1": 1, "arg2": 2, "arg3": {"arg3_1":3}}
+
+    test = dictToObj(test)
+
+    print(test.arg1)
+    print(test.arg2)
+    print(test.arg3)
+    print(test.arg3.arg3_1)
